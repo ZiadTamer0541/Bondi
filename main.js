65864b97824eab1763efbaed37cb6e19b16f3e61
ZiadTamer0541 <149613663+ZiadTamer0541@users.noreply.github.com>
2024-06-29 15:21:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// let Featuers = document.getElementById("Featuers")
+// let arrow = document.getElementById("arrow")
+// let arrow_2 = document.getElementById("arrow-2")
+// let Company = document.getElementById("Company")
+
+// Featuers.addEventListener('click', () => {
+//   if (arrow.classList.contains('up')) {
+//       arrow.classList.remove('up');
+//       arrow.style.borderTop = ".3em solid" // Arrow down
+//       arrow.style.borderBottom = "0" // Arrow down
+//   } else {
+//       arrow.classList.add('up');
+//       arrow.style.borderBottom = ".3em solid" // Arrow down
+//       arrow.style.borderTop = "0" // Arrow up
+//   }
+// });
+// Company.addEventListener('click', () => {
+//   if (arrow_2.classList.contains('up')) {
+//       arrow_2.classList.remove('up');
+//       arrow_2.style.borderTop = ".3em solid" // Arrow down
+//       arrow_2.style.borderBottom = "0" // Arrow down
+//   } else {
+//       arrow_2.classList.add('up');
+//       arrow_2.style.borderBottom = ".3em solid" // Arrow down
+//       arrow_2.style.borderTop = "0" // Arrow up
+//   }
+// });
